b6a7518dc7085c5a6b1e7eb86544db49c23eca25
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-04-18 21:42:32 +0300

    feat: add Point3d as derived class of Point2D

---
+++ src/gr/aueb/cf/cf9/ch14/Point3D.java
@@ -0,0 +1,36 @@
+package gr.aueb.cf.cf9.ch14;
+
+public class Point3D extends Point2D{
+    private double z;
+
+    public Point3D() {
+//        super();
+//        z = 0.0;
+
+    }
+
+    public Point3D(double x, double y, double z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    protected void movePls10() {
+        super.movePls10();
+        z += 1;
+    }
+
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        z += 10;
+    }
+}
